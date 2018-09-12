bd3595f67b4adee9e7b8c7932acad61029badd7a
Ethel C <cofresi.ethel@gmail.com>
2018-09-12 01:59:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-#	get '/' do
-#		erb :index  
-#	end
+	get '/' do
+		erb :index  
+	end
+
   get '/info' do 
      erb :info
   end
